12105e8252a7ffc28d9c29e4314120afac721c66
Amelialorite <amelialorite@gmail.com>
2019-01-16 20:10:07 +0100

    First commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
+import {MatToolbarModule,
+        MatButtonModule
+      } from '@angular/material';
+import { EquipoComponent } from './equipo/equipo.component';
+import { JugadorComponent } from './jugador/jugador.component';
+import { LigaComponent } from './liga/liga.component';
+import { LoginComponent } from './login/login.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EquipoComponent,
+    JugadorComponent,
+    LigaComponent,
+    LoginComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MatToolbarModule,
+    MatButtonModule
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
